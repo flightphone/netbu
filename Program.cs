d400ff0ffc30cccd079854b5129568f0bd3a620e
Efim Manevich <efimmanevich@gmail.com>
2019-06-18 05:03:14 +0400

    Timer

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -21,18 +22,18 @@ namespace netbu
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
-        var config = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("global.json")
-            .Build();
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("global.json")
+                .Build();
 
-        AppConfig = config;
-        isPostgres = (AppConfig["isP"] == "postgres");
-        string proxy = AppConfig["proxy"];
-        string proxyport = AppConfig["proxyport"];
-        string proxylogin = AppConfig["proxylogin"];
-        string proxypassword = AppConfig["proxypassword"];
-        if (!string.IsNullOrEmpty(AppConfig["proxy"]))
+            AppConfig = config;
+            isPostgres = (AppConfig["isP"] == "postgres");
+            string proxy = AppConfig["proxy"];
+            string proxyport = AppConfig["proxyport"];
+            string proxylogin = AppConfig["proxylogin"];
+            string proxypassword = AppConfig["proxypassword"];
+            if (!string.IsNullOrEmpty(AppConfig["proxy"]))
             {
                 WebProxy wp = new WebProxy(proxy, int.Parse(proxyport));
                 if (string.IsNullOrEmpty(proxylogin))
@@ -42,26 +43,47 @@ namespace netbu
                 WebRequest.DefaultWebProxy = wp;
                 //GlobalProxySelection.Select = wp;
             }
-        //Мапим диск
-        try{
+            //Мапим диск
+            try
+            {
                 System.Diagnostics.Process batch = new System.Diagnostics.Process();
                 batch.StartInfo.FileName = @"wwwroot\Run\disk.cmd";
-                batch.Start();  
+                batch.Start();
+
+                timer = new Timer(
+                callback: new TimerCallback(TimerTask),
+                state: null,
+                dueTime: 60000,
+                period: 86400000
+                );
+            }
+            catch
+            {; }
+
+
+
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(config)
+                .UseStartup<Startup>();
         }
-        catch
-        {;}        
-        //https://stackoverflow.com/questions/4624113/start-a-net-process-as-a-different-user          
 
-
-
-        return WebHost.CreateDefaultBuilder(args)
-            .UseConfiguration(config)
-            .UseStartup<Startup>();
+        private static Timer timer;
+        private static void TimerTask(object timerState)
+        {
+            try
+            {
+                System.Diagnostics.Process batch = new System.Diagnostics.Process();
+                batch.StartInfo.FileName = @"C:\Projects2018\task.cmd";
+                batch.Start();
+            }
+            catch
+            {; }
         }
 
         public static IConfiguration AppConfig { get; set; }
 
-        public static bool isPostgres {get;set;}
+        public static bool isPostgres { get; set; }
         public static bool FlagDadataUpdate = false;
     }
 }
---
+++ start.cmd
@@ -0,0 +1,3 @@
+cd C:\Projects19\netbu
+start http://127.0.0.1:5000
+dotnet run
